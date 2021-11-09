06d813f12228a5bf7987db228b5545641020785b
Dimitar Georgiev <dimitar.georgiev.bg@gmail.com>
2021-11-09 14:51:01 +0200

    With intermediate data types

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -4,10 +4,19 @@
       import skunk.codec.all._
 
       object Test {
+
+        final case class Foo(a: String, b: String, c: String)
+        final case class Bar(a: String, b: String, c: String, d: String, e: String, f: String)
+
+
+        val fooCodec = (text ~ text ~ text).imap{ case a ~ b  ~ c => Foo(a, b, c) }(x => x.a ~ x.b ~ x.c)
+        val barCodec = (text ~ text ~ text ~ text ~ text ~ text).imap{ case a ~ b  ~ c ~ d ~ e ~ f => Bar(a, b, c, d, e, f) }(x => x.a ~ x.b ~ x.c ~ x.d ~ x.e ~ x.f)
+
+
         (
-          text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text ~ text
+          fooCodec ~ barCodec ~ barCodec ~ fooCodec ~ fooCodec ~ barCodec ~ fooCodec ~ barCodec // 37 cols
         ).map {
-        case _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ ~ _ => ()
+        case foo ~ bar1 ~ bar2 ~ foo2 ~ foo3 ~ bar3 ~ foo4 ~ bar4 => ()
         }
       }
       
